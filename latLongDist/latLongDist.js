d09f92715823dc71aa78a42cf60877259a907529
Nathan Sass <nsass711@gmail.com>
2014-06-04 20:45:38 -0700

    have a good working solution with comments

--- latLongDist/latLongDist.js
+++ latLongDist/latLongDist.js
@@ -5,51 +5,52 @@
 //    that the line of code serves.
 
 
-var findDistHaversine = (function(){
-	var RADIUS_KM = 6371;
+var findDistHaversine = (function(){          
+	var RADIUS_KM = 6371;                                                         // I made this capital because it is a constant
 	
-	var _toRadians = function(coord){
+	var _toRadians = function(coord){                                             // I usually prefix my helper functions with '_'
 		return coord * Math.PI / 180;
 	};
 
-	var _haversineLogic = function(dLati, dLongi, lati1, lati2){
-		 var a = Math.sin(dLati/2) * Math.sin(dLati/2) +
-             Math.sin(dLongi/2) * Math.sin(dLongi/2) *
-             Math.cos(lati1) * Math.cos(lati2);
-     var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-     return RADIUS_KM * c;
+	var _haversineLogic = function(coordObj){                                     // Although I very much enjoyed reading
+		var a = Math.sin(coordObj.dLat/2) * Math.sin(coordObj.dLat/2) +             // about this formula on wikipedia, I elected
+		       Math.sin(coordObj.dLong/2) * Math.sin(coordObj.dLong/2) *            // to keep it intact including the variable
+		       Math.cos(coordObj.lat1) * Math.cos(coordObj.lat2);                   // naming conventions.
+		var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+		return RADIUS_KM * c;
 	};
 
 	return function haversine(coords){
-		var lat1  = _toRadians(coords.initalPos.latitude);
-		var long1 = _toRadians(coords.initalPos.longitude);
-		var lat2  = _toRadians(coords.endPos.latitude);
-		var long2 = _toRadians(coords.endPos.longitude);
-		var dLat  = lat2  - lat1;
-		var dLong = long2 - long1;
+		var coordObj = {
+			
+			lat1:  _toRadians(coords.initalPos.latitude),                            // Although I considered just passing the coords
+			long1: _toRadians(coords.initalPos.longitude),                           // object into a helper function and have it sort
+			lat2:  _toRadians(coords.endPos.latitude),                               // out the details, I decided to leave it in the
+			long2: _toRadians(coords.endPos.longitude),                              // method because I realize that my coordObj is 
+			dLat:  _toRadians(coords.endPos.latitude  - coords.initalPos.latitude),  // not the most efficient (read more below) and 
+			dLong: _toRadians(coords.endPos.longitude - coords.initalPos.longitude), // I wanted the next person changin this code
+			                                                                         // to notice that.
+		}
 
-    return _haversineLogic(dLat, dLong, lat1, lat2);
+    return _haversineLogic(coordObj);                
 	}
 })();
 
 
-
-
-var coordObj = {
-	initalPos: {
-		latitude: 37.77,
-		longitude: -122.41,
-	},
+var coordObj = {                                                                // I chose to represent the data in this way 
+	initalPos: {                                                                  // because of a method in my LocalHUNT app
+		latitude: 37.77,                                                            // that packages data in a similar way as this.
+		longitude: -122.41,                                                         // I wanted to pretend that I was working on a
+	},                                                                            // code base.
 	endPos: {
 		latitude: 38.90,
 		longitude: -77.03,
 	}
 }
 
-console.log(findDistHaversine(coordObj))
+console.log(findDistHaversine(coordObj))                                        // I wanted to pass in an object here
+                                                                                // instead of a big collection of numbers
 
 
-
-
-// https://github.com/niix/haversine/blob/master/haversine.js
-
+// https://github.com/niix/haversine/blob/master/haversine.js                       I liked this guy's start, but I think he should have considered scope more.
+// http://www.movable-type.co.uk/scripts/latlong.html                               This was a good resource for checking my work
