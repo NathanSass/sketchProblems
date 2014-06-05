b83cd4a9fc75061a9069c607e645adae8b6d5ebd
Nathan Sass <nsass711@gmail.com>
2014-06-05 12:27:54 -0700

    able to sort the list, TODO clean up naming

--- dragDrop/app.js
+++ dragDrop/app.js
@@ -1,3 +1,4 @@
+
 var source;
 
 function dragStarted(evt){
@@ -25,3 +26,47 @@ function dropped(evt){
 	//update text in drop target
 	evt.target.innerHTML = evt.dataTransfer.getData("text/plain");
 }
+
+function addListItem(){
+	event.preventDefault();
+	var inputText = $('input').val();
+	buildListItem(inputText);
+
+}
+
+function buildListItem(text){
+	var template = $('#template').clone().html(text).removeAttr('id').show();
+	$('ul').prepend(template)
+}
+
+function sortListItems(){
+	event.preventDefault();
+	var alphaOrdered = []
+	var currentList = $('ul li')
+	currentList.sort(function(a, b){
+		var keyA = $(a).text();
+	  var keyB = $(b).text();
+
+	  if (keyA.toLowerCase() < keyB.toLowerCase()) return -1;
+    if (keyA.toLowerCase() > keyB.toLowerCase()) return 1;
+    return 0;
+	});
+	$.each(currentList, function(i, li){
+		$('ul').append(li)
+	})
+}
+
+function bindListeners(){
+	$('.submit').click(addListItem);
+	$('.sort').click(sortListItems);
+};
+
+
+$( document ).ready(function(){
+	
+	bindListeners()
+
+});
+
+
+
