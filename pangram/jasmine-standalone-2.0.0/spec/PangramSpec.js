3e00994c74ebc3a2e19d262135aa47bd7924fb54
Nathan Sass <nsass711@gmail.com>
2014-06-06 15:34:25 -0700

    removed older files, have afully functioning function and passing tests

--- pangram/jasmine-standalone-2.0.0/spec/PangramSpec.js
+++ pangram/jasmine-standalone-2.0.0/spec/PangramSpec.js
@@ -1,9 +1,29 @@
 describe("pangramChecker", function(){
-	it("should return null for a pangram", function(){
-		var phrase = 'I am Nathan'
-		var response = []
-		pangramChecker('abcd');
-		expect(pangramChecker(phrase)).toEqual(response)
+	var phrase;
+	
+	it("should return null for a pangram with all lowercase", function(){
+		phrase = "a quick brown fox jumps over the lazy dog"
+		expect(pangramChecker(phrase)).toEqual(null)
+	});
+
+	it("should return null for a panagram with mixedcase", function(){
+		phrase = "A quick brOWn fox jumps over the LAzy DOG"
+		expect(pangramChecker(phrase)).toEqual(null)
+	})
+
+	it("should return 26 letters for an empty phrase",function(){
+		phrase = " "
+		expect(pangramChecker(phrase).length).toEqual(26)
+	})
+
+	it("should return 20 letters for a phrase with 6 unique letters",function(){
+		phrase = "abc def"
+		expect(pangramChecker(phrase).length).toEqual(20)
+	})
+
+	it("should not double count repeated letters in a phrase",function(){
+		phrase = "aaaAAAAA"
+		expect(pangramChecker(phrase).length).toEqual(25)
 	})
 
 })
