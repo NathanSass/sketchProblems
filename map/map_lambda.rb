046bf5c4f5fd2bc60783297beef3efb1b9fb2a0f
Nathan Sass <nsass711@gmail.com>
2014-06-11 07:27:27 -0700

    added lambda map

--- map/map_lambda.rb
+++ map/map_lambda.rb
@@ -1,1 +1,16 @@
 times_two = lambda { |x| x * 2}
+
+class Array
+	def new_map(func)
+		result = []
+		i = 0
+		while i < self.length
+			result << func.call(self[i])
+			i += 1
+		end
+		return result
+	end
+end
+
+arr = [1, 2, 3]
+p arr.new_map(times_two)
