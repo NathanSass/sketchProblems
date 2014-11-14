b5a88d1532e8e1061782fd7aca3b86fa58463777
E750606 <nathan.sass@chase.com>
2014-11-14 10:03:29 -0800

    alphabetize a word

---
+++ algorithms/alphabetWord.js
@@ -0,0 +1,21 @@
+var word = ["d", "y", "x", "f", "b", "s"]
+
+function alphabetize(arr){
+	var notSorted = true;
+
+	while(notSorted){
+		notSorted = false
+		for(var i=0; i < arr.length; i++){
+			var first = arr[i]
+			var second = arr[i + 1]
+			if(first > second){
+				arr[i] = second
+				arr[i + 1] = first
+				notSorted = true
+			}
+		}
+	}
+	return arr
+}
+
+console.log(alphabetize(word))
