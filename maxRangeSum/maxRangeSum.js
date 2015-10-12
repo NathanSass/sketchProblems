4737fe460ad69c384423dd27762dda4271e0d97f
Nathan Sass <nsass711@gmail.com>
2015-10-12 05:56:56 -0700

    added sanitizations for data file, ready to write method

--- maxRangeSum/maxRangeSum.js
+++ maxRangeSum/maxRangeSum.js
@@ -1,32 +1,37 @@
-// function maxEarnings(arr) {
-// 	var max = 0;
+function maxEarnings(arr) {
+	var max = 0;
 
-// 	arr.forEach(function(startDay, startI, dayArr){
-// 		var currentRun = startDay;
+	arr.forEach(function(startDay, startI, dayArr){
+		var currentRun = startDay;
 
-// 		for( var day = startI + 1; day < dayArr.length; day++) {
-// 			currentRun += dayArr[day];
-// 			if (currentRun > max){
-// 				max = currentRun;
-// 			}
-// 		}
+		for( var day = startI + 1; day < dayArr.length; day++) {
+			currentRun += dayArr[day];
+			if (currentRun > max){
+				max = currentRun;
+			}
+		}
 
-// 	});
+	});
 
-// 	return max;
-// }
-
-// var arr1 = [7, -3, -10, 4, 2, 8, -2, 4, -5, -2];
-
-// console.log(maxEarnings(arr1));
+	return max;
+}
 
 
 var fs  = require("fs");
 
 fs.readFileSync('./inputData.txt').toString().split('\n').forEach(function (line) {
-    if (line != "") {
-        //do something here
-        console.log(line);
+    if (line !== "") {
+	var stockMarket =  line.split(' ');
+	var unMashItems = stockMarket.shift().split(';');
+
+	var numDays		= unMashItems.shift();
+
+	stockMarket = unMashItems.concat(stockMarket)
+							.map(function(num){
+								return parseInt(num, 10);
+							});
+        console.log(numDays, ' --- ',stockMarket);
+        // console.log(numDays, '  line: ', line);
     }
 });
 
