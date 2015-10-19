2083fede110f2e17aba95ca64fbc5121ad6467dd
Nathan Sass <nsass711@gmail.com>
2015-10-19 16:42:40 -0700

    added gitignore

---
+++ reverseString/reverseString.java
@@ -0,0 +1,32 @@
+/**
+ * Created by nathansass on 10/19/15.
+ */
+
+
+public class reverseString {
+
+    public static void main(String[] args) {
+        System.out.println("Reversed Strings:");
+
+        String[] stringsToReverse = {
+            "Hello",
+            "World!"
+        };
+
+        for(String s: stringsToReverse) {
+
+            String reversedString = "";
+            String[] wordArr = s.split("");
+            for(int i = wordArr.length - 1; i>= 0; i-- ) {
+                reversedString += wordArr[i];
+
+            }
+
+            System.out.printf("%s : %s \n", s, reversedString);
+            System.out.println("Bye!");
+        }
+
+
+
+    }
+}
---
+++ reverseWord/ReverseWord.java
@@ -0,0 +1,5 @@
+/**
+ * Created by nathansass on 10/19/15.
+ */
+public class ReverseWord {
+}
