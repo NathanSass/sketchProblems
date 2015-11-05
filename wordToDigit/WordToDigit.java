6ec34c01718b285c99d472813aee872deb0ff833
Nathan Sass <nsass711@gmail.com>
2015-11-05 14:31:20 -0800

    initial commit, new challenge

---
+++ wordToDigit/WordToDigit.java
@@ -0,0 +1,22 @@
+import java.lang.reflect.Array;
+
+/**
+ * Created by nathansass on 11/5/15.
+ *
+ * Challenge from
+ * https://www.codeeval.com/open_challenges/104/
+ */
+
+public class WordToDigit {
+
+    final String[] wordNumbers = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}
+
+    private static void wordToDigit(String word) {
+    }
+
+
+    public static void main(String[] args) {
+        String word = "zero;two;five;seven;eight;four";
+        wordToDigit(word);
+    }
+}
