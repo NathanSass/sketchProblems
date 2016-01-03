97e56521d14e75e4a58b3978f9753b745907e6d5
Nathan Sass <nsass711@gmail.com>
2016-01-03 14:59:10 +1100

    adding setup for detecting Cycles

---
+++ detectingCycles/detectingCycles.js
@@ -0,0 +1,12 @@
+var charArrays = [[ '2', '0', '6', '3', '1', '6', '3', '1', '6', '3', '1' ],[ '3','4','8','0','11','9','7','2','5','6','10','1','49','49','49','49' ],[ '1', '2', '3', '1', '2', '3', '1', '2', '3' ]];
+// var fs = require("fs");
+// fs.readFileSync('./data.txt').toString().split('\n').forEach(function(line){
+	// if (line !== "") {
+		// var charArray = line.split(/\s+/);
+charArrays.forEach(function(line) {
+
+
+		console.log(line);
+})
+	// }
+// });
