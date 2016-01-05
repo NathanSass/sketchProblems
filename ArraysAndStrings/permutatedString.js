610177e370c18028002b70615c77c05899473646
Nathan Sass <nsass711@gmail.com>
2016-01-05 12:56:37 +1100

    very simple string equality problem

--- ArraysAndStrings/permutatedString.js
+++ ArraysAndStrings/permutatedString.js
@@ -3,35 +3,18 @@
 // https://www.reddit.com/r/dailyprogrammer/comments/164zvs/010713_challenge_116_easy_permutation_of_a_string/
 // p.73
 
-function allPerms(string) {
-    
-    function recur(string, prefix) {
-        if (string.length === 0) {
-            return [prefix];
-        } else {
-            var out = [];
-            for (var i = 0; i < string.length; i++) {
-                var pre = string.substring(0, i);
-                var post = string.substring(i + 1);
-                out = out.concat(recur(pre + post, string[i] + prefix));
-            }
-            console.log("out: ", out)
-            return out;
-        }
-    }
-    
-    var distinct = {};
-    
-    // return recur(string, "")
-    
-    recur(string, "").forEach(function(result) {
-        distinct[result] = true;
-    });
-    
-    // return distinct;
-    return Object.keys(distinct);
+
+
+function isPermut (str1, str2) {
+	if (str1.length !== str2.length) return false;
+	
+	var string1 = orderStr(str1);
+	var string2 = orderStr(str2);
+	return string1 === string2;
 }
 
-console.log( "final: ", allPerms('abc') );
+function orderStr(str) {
+	return str.toLowerCase().split('').sort().join('');
+}
 
-
+console.log( isPermut("abcd", "cabd") );
