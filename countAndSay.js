58a71cd41abfa3847af3855200e78cc42038b1a5
nathan <nathans@scribd.com>
2017-07-16 13:40:32 -0700

    working solution

---
+++ countAndSay.js
@@ -0,0 +1,47 @@
+/*
+https://leetcode.com/problems/count-and-say/#/description
+
+n String to print 
+0 1 
+1 1 1 
+2 2 1 
+3 1 2 1 1 
+*/
+
+var results = [1]
+
+function getElement(n) {
+	while (results.length < n + 1) {
+		var lastItem = results[results.length - 1];
+		var nextElement = getNextElement(lastItem);
+		results.push(nextElement);
+	}
+
+	return results[n - 1];
+}
+
+function getNextElement(x) {
+	var prev = x + "";
+	var result = "";
+	var prevChar = prev[0];
+	var currentChar = prev[0];
+	var count = 1;
+	for (var i = 1; i < prev.length; i++) {
+		currentChar = prev[i];
+		if (prevChar == currentChar) {
+			count ++
+		} else {
+			result += count;
+			result += prevChar;
+			count = 1;
+			prevChar = currentChar;
+		}
+	}
+
+	result += count;
+	result += currentChar;
+
+	return result;
+}
+
+console.log(getNextElement(21))
